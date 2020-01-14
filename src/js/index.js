273e1745c8132349a4d48f8a7413b99636841495
Francisco Javier Tinoco Pérez <ftinoco@ia.com.mx>
2020-01-14 11:32:54 -0600

    Feat: develop entry and interaction only desktop device

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,47 @@
+import { TweenMax, TimelineMax, Expo } from 'gsap'
 
+// tools
+const isMobileDevice = () => new RegExp(/Mobi/i).test(window.navigator.userAgent)
+
+// logic
+const triggerEntry = () => {
+  new TimelineMax()
+    .staggerTo('.card__row-inner', 1, {
+      y: 0,
+      ease: Expo.easeOut
+    }, 0.1)
+}
+
+const windowLoadHandler = () => {
+  window.setTimeout(() => {
+    TweenMax
+      .to('.loader', 0.5, {
+        autoAlpha: 0,
+        ease: Expo.easeOut
+      })
+      .eventCallback('onComplete', triggerEntry)
+  }, 1000)
+}
+
+const mouseMoveHandler = ({ clientX, clientY }) => {
+  const x = (clientX / window.innerWidth) * 2 - 1
+  const y = (clientY / window.innerHeight) * 2 - 1
+  const factor = 5
+
+  TweenMax
+    .to('.card', 1, {
+      rotateY: x * factor,
+      rotateX: -y * factor
+    })
+}
+
+const setupInitState = () => {
+  TweenMax.set('.card__row-inner', { y: '100%' })
+}
+
+// bind events
+window.addEventListener('load', windowLoadHandler)
+if (!isMobileDevice()) window.addEventListener('mousemove', mouseMoveHandler)
+
+// init
+setupInitState()
